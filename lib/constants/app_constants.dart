87f33035150fd49b7f8621f98e6ba3f00e7ffdc1
Carlos Diaz <carlos.cedp@gmail.com>
2021-11-11 21:37:51 -0400

    🎉 project files and colors added

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const redColor = Color(0xFFE13E34);
+  static const blueColor = Color(0xFF2E8DFF);
+  static const lightBlueColor = Color(0xFF4D98F4);
+  static const greenColor = Color(0xFF20C764);
+  static const lightGreyColor = Color(0xFFF7F9FC);
+  static const greyColor = Color(0xFFeFF1F5);
+  static const titleTextColor = Color(0xFF212226);
+  static const subtitleTextColor = Color(0xFF999EA6);
+  static const yellowColor = Color(0xFFFFDE2C);
+}
